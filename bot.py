2756412d42c4895786650b864d283fb71d0efcf1
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-05-11 12:45:45 +0300

    work with user min/max coefficients

--- bot.py
+++ bot.py
@@ -2,26 +2,29 @@
 # -*- coding: utf-8 -*-
 
 
-
 import random
 import os
 
 
-from time import asctime 
+from time import asctime
+from typing import Text 
 from telebot import TeleBot
 from moduls.popanchik import popan_press_bot
 from moduls.popanchik import popan_list_bot
-from moduls.popanchik import contest_press_bot
-from moduls.popanchik import returnMatchesForPopanchik
-from moduls.start_hour import startHour
 
 
+COMMANDS_EXPRESS = 'экспресс *минимальный коэфициент экспресса* *миннимальный коэффициент матча* *максимальный коэффициент матча*\n\
+экспресс *минимальный коэфициент экспресса*\
+или просто слово "экспресс" для попанского экспресса\n\n'
+COMMANDS_MATCHES = 'матчи *миннимальный коэффициент матча* *максимальный коэффициент матча* - чтобы получить список всех попанский матчей\n\
+или просто слово "матчи" для списка матчей с коэфициентами по умолчанию'
 TOKEN = os.getenv('TOKEN')
 
 
 def log(file, msg):
     with open(file, 'a', encoding='UTF-8') as file:
         file.write(msg)
+
 
 def get_name(message):
     if message.chat.username is None:
@@ -30,13 +33,14 @@ def get_name(message):
         name = f'username {message.chat.username}'
     return name
 
+
 def bot():
     bot = TeleBot(TOKEN)
     #Ответ на команды /start и /help
     @bot.message_handler(commands=['start', 'help'])
     def handle_start_help(message):
         bot.reply_to(message, f'Привет {message.chat.first_name}\nЯ Пробити Кот и я могу дать тебе попанский пресс или список всех попанских матчей на сегодня\
-для этого  напиши команду /press или /matches.\n')
+для этого  напиши команду:{COMMANDS_EXPRESS}{COMMANDS_MATCHES}')
 
     # Обрабатывается голосовые сообщения
     @bot.message_handler(content_types=['voice'])
@@ -46,22 +50,41 @@ def bot():
     #Обработка картиночек
     @bot.message_handler(content_types=['photo'])
     def handle_photos(message):
-        answers = ['Nice meme xD', 'Ха ору :D', ')))', 'Давай еще :D']
+        answers = ['Ха ору :D', ')))', 'Давай еще :D']
         bot.reply_to(message, f'{random.choice(answers)}')
 
 
-    @bot.message_handler(commands=['press'])
+    @bot.message_handler(func=lambda message: 'прес' in message.text.lower(), content_types=['text'])
     def popan_press(message):
         name = get_name(message)
-        log('history.log', f'{asctime()} {name} спросил пресс\n')        
-        msg, press = popan_press_bot()
-        bot.reply_to(message, msg)
-
+        log('history.log', f'{asctime()} {name} спросил экспресс - {message.text}\n')     
+        try:
+            if (len(message.text.split()) == 4): # 4 arguments command, minCoefOfExpress, minCoefOfMatch, maxCoefOfMatch 
+                minCoefOfExpress = float(message.text.split()[1].replace(',', '.'))
+                minCoefOfMatch = float(message.text.split()[2].replace(',', '.'))
+                maxCoefOfMatch = float(message.text.split()[3].replace(',', '.'))
+                msg, press = popan_press_bot(minCoefOfExpress, minCoefOfMatch, maxCoefOfMatch)
+            elif (len(message.text.split()) == 2): # 2 arguments command and minCoefOfExpress
+                    minCoefOfExpress = float(message.text.split()[1].replace(',', '.'))
+                    msg, press = popan_press_bot(minCoefOfExpress)
+            elif (len(message.text.split()) == 1):
+                msg, press = popan_press_bot()
+            else:
+                raise ValueError('Ошибка в количестве аргументов')
+        except ValueError as error_msg:
+            msg = f"{error_msg}\n\nВведите команду в формате:\n{COMMANDS_EXPRESS}"
+            press = [f"Не удалось собрать экспресс по заданным параметрам {error_msg}"]
+        finally:
+            bot.reply_to(message, msg)
+        
+        
         log('history.log', f'{asctime()} пресс отправлен\n')
-        msg = str()
+        logMsg = str()
         for line in press:
-            msg += line + '\n'
-        log('history.log', msg + "\n")
+            logMsg += line + '\n'
+        log('history.log', logMsg + "\n")
+        
+            
 
 
     # @bot.message_handler(commands=['contest'])
@@ -79,11 +102,20 @@ def bot():
 
 
 
-    @bot.message_handler(commands=['matches'])
+    @bot.message_handler(func=lambda message: 'матчи' in message.text.lower(), content_types=['text'])
     def matches_list(message):
         name = get_name(message)
         log('history.log', f'{asctime()} {name} спросил список матчей\n')
-        msg = popan_list_bot()
+        try:
+            if len(message.text.split()) == 3: # 3 arguments command, minCoefOfMatch, maxCoefOfMatch
+                minCoefOfMatch = float(message.text.split()[1].replace(',', '.'))
+                maxCoefOfMatch = float(message.text.split()[2].replace(',', '.'))
+                msg = popan_list_bot(minCoefOfMatch, maxCoefOfMatch)
+            elif len(message.text.split()) < 3:
+                msg = popan_list_bot()
+        except ValueError as error_msg:
+            msg = [f'Не удалось собрать матчи по данным параметрам\n{error_msg}\n\n{COMMANDS_MATCHES}']
+            
         if len(msg) == 1:
             bot.reply_to(message, msg[0])
         else:
@@ -93,17 +125,17 @@ def bot():
         name = get_name(message)
         log('history.log', f'{asctime()} список для {name} выдан\n')
 
+
     # Обрабатывается текстовые  сообщения
     @bot.message_handler(content_types=['text'])
     def handle_text(message):
         bot.reply_to(message, f'Извини, {message.chat.first_name}, но я пока не могу свободно общаться.\n\
 Чтобы получить прогнозы или список матчей нужно вводить команды:\n\
-/press - чтобы получить случайный попанский экспресс\n\n\
-/matches - чтобы получить список всех попанский матчей')
+{COMMANDS_EXPRESS}{COMMANDS_MATCHES}')
 
         name = get_name(message)
         log('history.log', f'{asctime()} {name} написал:\n {message.text}\n\n')
 
 
 
-    bot.polling(interval=5, timeout=30, long_polling_timeout=40)
+    bot.polling()
--- moduls/popanchik.py
+++ moduls/popanchik.py
@@ -7,12 +7,13 @@ import time
 from moduls.start_hour import startHour
 from moduls.read_from_livescore import load_obj
 
-MINKF = 1.40
-MAXKF = 1.75
+MIN_COEF = 1.40
+MAX_COEF = 1.75
+MIN_COEF_EXPRESS = 2.5
 
 
-def returnMatchesForPopanchik(matches, hour, mink, maxk):
-    '''выбираются матчи с кефом на фаворита между MINKF и MAXKF, начнутся через 2 и более часов, не женских и не юниорских команд.'''
+def returnMatchesForPopanchik(matches, mink=MIN_COEF, maxk=MAX_COEF, hour=startHour()):
+    '''выбираются матчи с кефом на фаворита между MIN_COEF и MAX_COEF, начнутся через 2 и более часов, не женских и не юниорских команд.'''
     popanmatches = [] 
     for line in matches:
         if all([(mink <= line["kw1"] <= maxk or mink <= line["kw2"] <= maxk),
@@ -23,59 +24,62 @@ def returnMatchesForPopanchik(matches, hour, mink, maxk):
     return popanmatches
 
 
-def getPopanPress(popanmatches):
-    press = []
+def getPopanPress(minCoefOfExpress, minCoefOfMatch, maxCoefOfMatch):
+    popanmatches, day = get_popan_matches_list(minCoefOfMatch, maxCoefOfMatch)
     coef = 1
-    a = 0
-    ligth_press = random.randint(2,3)
-    if ligth_press > (len(popanmatches) - len(usedmatches)):
-        return []
-
-    while a < ligth_press:
+    usedmatches = []
+    press = []
+    while coef < minCoefOfExpress:
+        if (len(popanmatches) - len(usedmatches) == 0):
+            return 
         i = random.randint(0, len(popanmatches) - 1)
         if popanmatches[i] not in usedmatches:
             usedmatches.append(popanmatches[i])
-            a += 1
             if popanmatches[i]["kw1"] < popanmatches[i]["kw2"]:
-                press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
-{popanmatches[i]["team1"]} - {popanmatches[i]["team2"]} П1 кф. {popanmatches[i]["kw1"]}')
+                press.append(f'\
+{popanmatches[i]["country"]} \
+{popanmatches[i]["time"]} \
+{popanmatches[i]["team1"]} - \
+{popanmatches[i]["team2"]} П1 кф. \
+{popanmatches[i]["kw1"]}')
                 coef *= popanmatches[i]["kw1"]
             else:
-                press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
-{popanmatches[i]["team1"]} - {popanmatches[i]["team2"]} П2 кф. {popanmatches[i]["kw2"]}')
+                press.append(f'\
+{popanmatches[i]["country"]} \
+{popanmatches[i]["time"]} \
+{popanmatches[i]["team1"]} - \
+{popanmatches[i]["team2"]} П2 кф. \
+{popanmatches[i]["kw2"]}')
                 coef *= popanmatches[i]["kw2"]
-
+    
     press.append(f"Итоговый кф {round(coef, 2)}")
-    usedmatches.clear()
     return press
 
-def get_popan_matches_list():
-    hour = startHour()
+
+def get_popan_matches_list(minCoefOfMatch, maxCoefOfMatch):
     day = 'today'
     matches_dict = load_obj(day)
-    popmatches = returnMatchesForPopanchik(matches_dict, hour, MINKF, MAXKF)
+    popmatches = returnMatchesForPopanchik(matches_dict, minCoefOfMatch, maxCoefOfMatch)
     popmatches = sorted(popmatches, key=lambda k: k['time']) 
-    #print(popmatches)
-    #print(len(popmatches))
     if len(popmatches) < 3:
         hour = 0
         day = 'tomorrow'
         matches_dict = load_obj(day)
-        popmatches = returnMatchesForPopanchik(matches_dict, hour, MINKF, MAXKF)
+        popmatches = returnMatchesForPopanchik(matches_dict, minCoefOfMatch, maxCoefOfMatch)
         popmatches = sorted(popmatches, key=lambda k: k['time'])
-        #print(popmatches)
-        #print(len(popmatches))
     return popmatches, day
 
 
-def popan_press_bot():
-    popmatches, day = get_popan_matches_list()
+def popan_press_bot(minCoefOfExpress = MIN_COEF_EXPRESS, minCoefOfMatch = MIN_COEF, maxCoefOfMatch = MAX_COEF):
+    msg = f'Собираем экспресс с минимальным коэфициентом {minCoefOfExpress}\n\
+Матчи с коэфициентами на фаворита от {minCoefOfMatch} до {maxCoefOfMatch} \n\n'
+    popmatches, day = get_popan_matches_list(minCoefOfMatch, maxCoefOfMatch)
     if day == 'tomorrow':
-        msg = f'На сегодня с матчами туго, давай посмотрим матчи на завтра\n\
+        msg += f'На сегодня с матчами туго, давай посмотрим матчи на завтра\n\
 Всего "попанских" матчей: {len(popmatches)}\n'
-    else: msg = f'Всего "попанских" матчей на сегодня {len(popmatches)}\n'
+    else: msg += f'Всего "попанских" матчей на сегодня {len(popmatches)}\n'
     if len(popmatches) >= 3:
-        press = getPopanPress(popmatches)
+        press = getPopanPress(minCoefOfExpress, minCoefOfMatch, maxCoefOfMatch)
         if len(press) < 1:
             msg += ('Возможно произошла ошибка при загрузке матчей, она повторяется каждые 10 минут')
         else:
@@ -90,14 +94,18 @@ def popan_press_bot():
     return msg, press
 
 
-def popan_list_bot():
-    popmatches, day = get_popan_matches_list()
-    msg = ['']
+def popan_list_bot(minCoefOfMatch=MIN_COEF, maxCoefOfMatch=MAX_COEF):
+    if minCoefOfMatch >= maxCoefOfMatch:
+        raise ValueError('Минимальный коэфициент не может быть больше или равен максимальному')
+    elif minCoefOfMatch > 5 or maxCoefOfMatch > 5:
+        raise ValueError('Таких коэффициентов на фаворита обычно не бывает')
+    popmatches, day = get_popan_matches_list(minCoefOfMatch, maxCoefOfMatch)
+    msg = [f'Собираем список матчей с кефами на фаворита от {minCoefOfMatch} до {maxCoefOfMatch} \n\n']
     i = 0
     if day == 'tomorrow':
-        msg[i] = f'На сегодня с матчами туго, давай посмотрим матчи на завтра\n\
-Всего "попанских" матчей на завтра: {len(popmatches)}\n'
-    else: msg[i] = f'Всего "попанских" матчей на сегодня: {len(popmatches)}\n\n'
+        msg[i] += f'На сегодня с матчами туго, давай посмотрим матчи на завтра\n\
+Всего матчей на завтра: {len(popmatches)}\n'
+    else: msg[i] += f'Всего матчей на сегодня: {len(popmatches)}\n\n'
     for ind, match in enumerate(popmatches):
         if match["kw1"] < match["kw2"]:
             msg[i] += (f'{match["time"]} {match["country"]} {match["team1"]} - {match["team2"]} П1 кф. {match["kw1"]} \n\n')
@@ -118,7 +126,7 @@ def contest_press_bot():
     day = 'contest'
     hour = 0
     matches_dict = load_obj(day)
-    popmatches = returnMatchesForPopanchik(matches_dict, hour, MINKF, MAXKF)
+    popmatches = returnMatchesForPopanchik(matches_dict, hour, MIN_COEF, MAX_COEF)
     msg = f'Всего "попанских" матчей на конкурс: {len(popmatches)}\n\n'
     if len(popmatches) >= 3:
         press = getPopanPress(popmatches)
